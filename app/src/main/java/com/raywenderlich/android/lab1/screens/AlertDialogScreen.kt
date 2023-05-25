4782669b6ee8ce64dc0704b6ceefa16ce6da94de
валерия <v.bobrovskaya0207@mail.ru>
2023-05-26 00:54:09 +0300

    AlertDialog

--- app/src/main/java/com/raywenderlich/android/lab1/screens/AlertDialogScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/AlertDialogScreen.kt
@@ -4,6 +4,16 @@ import androidx.compose.runtime.Composable
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.raywenderlich.android.lab1.R
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 
 @Composable
 fun AlertDialogScreen() {
@@ -16,6 +26,34 @@ fun AlertDialogScreen() {
 }
 
 @Composable
+@Preview
 fun MyAlertDialog() {
-    //TODO add your code here
+
+val shouldShowDialog = remember { mutableStateOf(true) }
+if (shouldShowDialog.value){
+    AlertDialog(
+        onDismissRequest = {
+            shouldShowDialog.value = false
+            FundamentalsRouter.navigateTo(Screen.Navigation)
+        },
+        title = { Text(text = stringResource(id = R.string.title_alertDialog))},
+        text = { Text(text = stringResource(id = R.string.alertDialog_about))},
+        confirmButton = {
+            Button(
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue),
+                onClick ={
+                    shouldShowDialog.value = false
+                    FundamentalsRouter.navigateTo(Screen.Navigation)
+                }
+            ) {
+                Text(
+                    text = stringResource(id = R.string.massage_button),
+                    color = Color.White
+                )
+            }
+        }
+
+    )
+
+}
 }
