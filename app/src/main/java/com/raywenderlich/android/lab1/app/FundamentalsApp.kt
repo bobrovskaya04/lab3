4371d46c447fd01b2fe9ed840952167e95fccb73
валерия <v.bobrovskaya0207@mail.ru>
2023-05-26 03:55:13 +0300

    Initial project setup

--- app/src/main/java/com/raywenderlich/android/lab1/app/FundamentalsApp.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/app/FundamentalsApp.kt
@@ -26,6 +26,9 @@ fun FundamentalsApp() {
                 is Screen.Scaffold -> ScaffoldScreen()
                 is Screen.Box -> BoxScreen()
                 is Screen.Surface -> SurfaceScreen()
+                is Screen.Scrolling -> ScrollingScreen()
+                is Screen.List -> ListScreen()
+                is Screen.Grid -> GridScreen()
             }
         }
     }
